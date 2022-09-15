7647ea6ac2acefbe076ce385c9ada828f8b2b9c7
Patrick Sherlock <info@psherlock.co.uk>
2022-09-15 22:00:10 +0100

    ADD: Mad animation moves

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,22 +1,54 @@
 <template>
 	<header class="c-header" ref="target">
-		<div class="c-header__border-top"></div>
-		<div class="c-header__container">
-			<ul class="c-header__nav c-header__nav--left">
-				<li>Menu</li>
-				<li>Whats On</li>
-				<li>Plan Your Visit</li>
-			</ul>
-			<img class="c-header__logo" src="~/assets/img/ALMEIDA_LOGO.svg"/>
-			<ul class="c-header__nav c-header__nav--right">
-				<li>Get Involved</li>
-				<li>Support Us</li>
-				<li>About Us</li>
-			</ul>
+		<div class="c-header__full-container">
+			<div class="c-header__container c-header__container--left">
+				<div class="c-header__border-top"></div>
+				<ul class="c-header__nav c-header__nav--left">
+					<li>Menu</li>
+					<li>Whats On</li>
+					<li>Plan Your Visit</li>
+				</ul>
+				<div class="c-header__border-bottom"></div>
+			</div>
+			<video muted playsinline class="c-header__logo" id="c-header__logo">
+				<source src="~/assets/img/ALM_Moving_Logo.mp4"
+		            type="video/mp4">
+			</video>
+			<div class="c-header__container c-header__container--right">
+				<div class="c-header__border-top"></div>
+				<ul class="c-header__nav c-header__nav--right">
+					<li>Get Involved</li>
+					<li>Support Us</li>
+					<li>About Us</li>
+				</ul>
+				<div class="c-header__border-bottom"></div>
+			</div>
 		</div>
-		<div class="c-header__border-bottom"></div>
 	</header>
 </template>
 <style scoped lang="scss">
 
 </style>
+
+<script>
+	export default {
+	  mounted () {
+		const startVideo = async () => {
+		  const video = document.querySelector('#c-header__logo');
+
+		  try {
+		      await video.play();
+
+		      video.setAttribute('autoplay', true);
+
+		      console.log('video started playing successfully');
+		  } catch (err) {
+		    console.log(err, 'video play error');
+		    // do stuff in case your video is unavailable to play/autoplay
+		  }
+		}
+
+		setTimeout(startVideo, 2000)
+	}
+}
+</script>
--- components/TheMasthead.vue
+++ components/TheMasthead.vue
@@ -1,11 +1,18 @@
 <template>
 	<div class="c-masthead">
-		<img 
+<!-- 		<img 
 			src="https://source.unsplash.com/random/1920x1080/?animal,landscape"
 			height="1000"
 			width="1600"
 			class="c-masthead__image"
-		>
+		> -->
+		<video autoplay muted playsinline loop class="c-masthead__video">
+
+		    <source src="~/assets/img/video.mp4"
+		            type="video/mp4">
+
+		    Sorry, your browser doesn't support embedded videos.
+		</video>
 		<div class="c-masthead__text o-wrapper">
 			<h1>Welcome</h1>
 			<h2>The Almeida exists to launch the next generation of British artists onto the world stage.</h2>
